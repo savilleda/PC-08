cbced691d0a6007d0f5a6a60c0891487b8170c31
PC-08 <savilleda@correo.url.edu.gt>
2025-03-25 09:40:17 -0600

    Mensaje de commit

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, mundo!");
+        }
+    }
+}
